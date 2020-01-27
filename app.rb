f4dd1e44e3a3e1791e5cb4d129029ce991c04e24
Jeffrey Brown <brownjs03@gmail.com>
2020-01-27 06:14:37 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -7,15 +7,15 @@ class App < Sinatra::Base
   end 
   
   get '/name' do 
-    "My name is __"
+    "My name is "
   end 
   
   get '/hometown' do 
-    "My hometown is __" 
+    "My hometown is " 
   end 
   
   get '/favorite-song' do 
-    "My favorite song is __"
+    "My favorite song is "
   end 
   
 end
